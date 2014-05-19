813bb85eb885b2991c451fa8eb1ed5225b29e498
yzhanguvm <yucanzhang@gmail.com>
2014-05-19 12:33:37 -0400

    Make a basic User model (including secure password)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
 	  @title = "Sign up"
   end
+
+  def create
+	  User.create(user_params)
+  end
+
+  private
+
+  def user_params
+	  params.require(:user).permit(:name, :email)
+  end
 end
